fc2afb68a9aafa16bfa9454274bb70f3df6ae07c
Anton Rusanov <assoulter123@gmail.com>
2012-06-13 23:38:32 +0400

    add twitter sharing

--- src/controller/codevault_cv_controller.erl
+++ src/controller/codevault_cv_controller.erl
@@ -7,14 +7,14 @@ index('GET', []) ->
 	
 parse('GET', []) ->
 	Data = Req:path(),
-	%%ShareUrl = Req:header(host) ++ Data, % full link here
+	ShareFullUrl = Req:header(host) ++ Data, % full link here
 	Split = lists:last(common_lib:split(Data, "/")),
 	Decoded = ("coderecord-" ++ integer_to_list(base62_lib:decode(Split))),
 	ShareUrl = "cdv.lt/" ++ Split,
 	Current = boss_db:find(Decoded),
 	case Current == undefined of
 		false ->
-			{ok, [{code_record, Current}, {share_url, ShareUrl}]};
+			{ok, [{code_record, Current}, {share_url, ShareUrl}, {current_url, ShareFullUrl}]};
 		true ->
 			{redirect, "/"}
 		end.
