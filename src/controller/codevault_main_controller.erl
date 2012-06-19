52ee4d45dd4ab230c29bfb2b1ef24e67d25a1694
Anton Rusanov <assoulter123@gmail.com>
2012-06-19 15:27:52 +0400

    oops

--- src/controller/codevault_main_controller.erl
+++ src/controller/codevault_main_controller.erl
@@ -26,7 +26,7 @@ sendData('POST', []) ->
 		{ok, SavedCodeRecord} ->
 			Split = lists:last(common_lib:split(SavedCodeRecord:id(), "-")),
 			CodeHash = base62_lib:encode(list_to_integer(Split)),
-			{json, [{result, CodeHash}]}
+			{json, [{result, CodeHash}]};
 		{error, ErrorList} ->
 			{json, [{result, "-1"}]}
 		end.
