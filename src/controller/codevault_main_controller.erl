615d9525dbb2bd89c8ba323124da1213ec2ba5f6
Anton Rusanov <assoulter123@gmail.com>
2012-06-19 15:24:32 +0400

    add sendData route

--- src/controller/codevault_main_controller.erl
+++ src/controller/codevault_main_controller.erl
@@ -17,3 +17,16 @@ index('POST', []) ->
 			{error, ErrorList} ->
 				{ok, [{errors, ErrorList}, {new_msg, NewCodeRecord}]}
 			end.
+
+sendData('POST', []) ->	
+	CodeText = string:strip(Req:post_param("code")),
+	CodeLang = Req:post_param("lang"),
+	NewCodeRecord = coderecord:new(id, CodeText, now(), CodeLang),
+	case NewCodeRecord:save() of
+		{ok, SavedCodeRecord} ->
+			Split = lists:last(common_lib:split(SavedCodeRecord:id(), "-")),
+			CodeHash = base62_lib:encode(list_to_integer(Split)),
+			{json, [{result, CodeHash}]}
+		{error, ErrorList} ->
+			{json, [{result, "-1"}]}
+		end.
