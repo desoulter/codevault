21c6a92fec925fc2b9547b27d3e625c9ff8ef647
Anton Rusanov <assoulter123@gmail.com>
2012-10-02 23:08:16 +0400

    snippets page

---
+++ src/controller/codevault_snippets_controller.erl
@@ -0,0 +1,9 @@
+-module (codevault_snippets_controller, [Req]).
+-compile (export_all).
+-default_action (index).
+
+index('GET', []) ->
+    Coderecords = boss_db:find(coderecord, 
+                            [{code_language, 'not_equals', "no-highlight"}], 
+                            [{limit, 20}, {offset, 0}, {order_by, code_time}, descending]),
+        {ok, [{coderecords, Coderecords}]}.
