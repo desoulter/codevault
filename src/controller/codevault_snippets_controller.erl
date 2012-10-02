fc74a77d4fa1cf84cd8b154f1b881f4a47dbef41
Anton Rusanov <assoulter123@gmail.com>
2012-10-02 23:30:27 +0400

    add custom filter for base62

--- src/controller/codevault_snippets_controller.erl
+++ src/controller/codevault_snippets_controller.erl
@@ -6,4 +6,5 @@ index('GET', []) ->
     Coderecords = boss_db:find(coderecord, 
                             [{code_language, 'not_equals', "no-highlight"}], 
                             [{limit, 20}, {offset, 0}, {order_by, code_time}, descending]),
+    
         {ok, [{coderecords, Coderecords}]}.
--- src/view/lib/filter_modules/codevault_custom_filters.erl
+++ src/view/lib/filter_modules/codevault_custom_filters.erl
@@ -7,3 +7,7 @@
 %     lists:reverse(binary_to_list(Value)).
 %
 % "foo"|my_reverse   => "foo"
+url_base62(Value) ->
+    Split = lists:last(common_lib:split(Value, "-")),
+    CodeHash = base62_lib:encode(list_to_integer(Split)),
+    CodeHash.
